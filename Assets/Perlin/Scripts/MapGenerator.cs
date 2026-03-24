4b8b5ed251850fec78413a81a3d820514eaa27e7
flopdebouwer <floris.bieling@hva.nl>
2026-03-24 14:37:33 +0200

    Add multithreaded map and mesh generation
    Replace synchronous map generation with background threads and callback dispatching. Add RequestMapData/RequestMeshData, thread workers, and queues (MapThreadInfo) plus a MapData struct; DrawMapInEditor now uses GenerateMapData. Make MeshGenerator thread-safe by cloning the AnimationCurve. Update EndlessTerrain to request map/mesh data asynchronously, use MeshRenderer/MeshFilter with a material instead of creating a primitive plane, and wire TerrainChunk to apply incoming mesh data. Small editor change: MapGeneratorEditor now calls DrawMapInEditor. Update Perlin.unity scene assets to include the new components and references.

--- Assets/Perlin/Scripts/MapGenerator.cs
+++ Assets/Perlin/Scripts/MapGenerator.cs
@@ -1,5 +1,8 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using System;
+using System.Threading;
+using System.Collections.Generic;
 
 public class MapGenerator : MonoBehaviour
 {
@@ -28,37 +31,103 @@ public class MapGenerator : MonoBehaviour
 
     public HeightTypes[] heightTypes;
 
-    public void GenerateMap()
+    Queue<MapThreadInfo<MapData>> mapDataThreadInfoQueue = new Queue<MapThreadInfo<MapData>>();
+    Queue<MapThreadInfo<MeshData>> meshDataThreadInfoQueue = new Queue<MapThreadInfo<MeshData>>();
+
+    public void DrawMapInEditor()
     {
-        float[,] heightMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, offset, redistributionPower);
-        float[,] temperatureMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed + 1, noiseScale, octaves, persistance, lacunarity, offset);
+        MapData mapData = GenerateMapData();
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
         if (drawMode == DrawMode.HeightNoiseMap)
         {
             display.DrawMesh(
-                MeshGenerator.GenerateTerrainMesh(heightMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail),
-                TextureGenerator.TextureFromHeightMap(heightMap)
+                MeshGenerator.GenerateTerrainMesh(mapData.heightMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail),
+                TextureGenerator.TextureFromHeightMap(mapData.heightMap)
                 );
-        } 
+        }
         else if (drawMode == DrawMode.TemperatureNoiseMap)
         {
             display.DrawMesh(
-                MeshGenerator.GenerateTerrainMesh(temperatureMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail),
-                TextureGenerator.TextureFromHeightMap(temperatureMap)
+                MeshGenerator.GenerateTerrainMesh(mapData.temperatureMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail),
+                TextureGenerator.TextureFromHeightMap(mapData.temperatureMap)
                 );
-        } 
+        }
         else if (drawMode == DrawMode.ColorMap)
         {
-            display.DrawTexture(TextureGenerator.TextureFromColorMap(CombineMaps(heightMap, temperatureMap), mapChunkSize, mapChunkSize));
+            display.DrawTexture(TextureGenerator.TextureFromColorMap(mapData.colorMap, mapChunkSize, mapChunkSize));
         }
         else if (drawMode == DrawMode.Mesh)
         {
             display.DrawMesh(
-                MeshGenerator.GenerateTerrainMesh(heightMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail),
-                TextureGenerator.TextureFromColorMap(CombineMaps(heightMap, temperatureMap), mapChunkSize, mapChunkSize)
+                MeshGenerator.GenerateTerrainMesh(mapData.heightMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail),
+                TextureGenerator.TextureFromColorMap(mapData.colorMap, mapChunkSize, mapChunkSize)
                 );
         }
+    }
+
+    public void RequestMapData(Action<MapData> callback)
+    {
+        ThreadStart threadStart = delegate
+        {
+            MapDataThread(callback);
+        };
+
+        new Thread(threadStart).Start();
+    }
+
+    void MapDataThread(Action<MapData> callback) {
+        MapData mapData = GenerateMapData();
+        lock (mapDataThreadInfoQueue)
+        {
+            mapDataThreadInfoQueue.Enqueue(new MapThreadInfo<MapData>(callback, mapData));
+        }
+    }
+
+    public void RequestMeshData(MapData mapData, Action<MeshData> callback)
+    {
+        ThreadStart threadStart = delegate
+        {
+            MeshDataThread(mapData, callback);
+        };
+        new Thread(threadStart).Start();
+    }
+
+    void MeshDataThread(MapData mapData, Action<MeshData> callback) {
+        MeshData meshData = MeshGenerator.GenerateTerrainMesh(mapData.heightMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail);
+        lock (meshDataThreadInfoQueue)
+        {
+            meshDataThreadInfoQueue.Enqueue(new MapThreadInfo<MeshData>(callback, meshData));
+        }
+    }
+
+    void Update()
+    {
+        if (mapDataThreadInfoQueue.Count > 0)
+        {
+            for (int i = 0; i < mapDataThreadInfoQueue.Count; i++)
+            {
+                MapThreadInfo<MapData> threadInfo = mapDataThreadInfoQueue.Dequeue();
+                threadInfo.callback(threadInfo.parameter);
+            }
+        }
+
+        if (meshDataThreadInfoQueue.Count > 0)
+        {
+            for (int i = 0; i < meshDataThreadInfoQueue.Count; i++)
+            {
+                MapThreadInfo<MeshData> threadInfo = meshDataThreadInfoQueue.Dequeue();
+                threadInfo.callback(threadInfo.parameter);
+            }
+        }
+    }
+
+    MapData GenerateMapData()
+    {
+        float[,] heightMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, offset, redistributionPower);
+        float[,] temperatureMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed + 1, noiseScale, octaves, persistance, lacunarity, offset);
+        
+        return new MapData(heightMap, temperatureMap, CombineMaps(heightMap, temperatureMap));
     }
 
     private void OnValidate()
@@ -70,6 +139,18 @@ public class MapGenerator : MonoBehaviour
         if (octaves < 0)
         {
             octaves = 0;
+        }
+    }
+
+    struct MapThreadInfo<T>
+    {
+        public readonly Action<T> callback;
+        public readonly T parameter;
+
+        public MapThreadInfo(Action<T> callback, T parameter)
+        {
+            this.callback = callback;
+            this.parameter = parameter;
         }
     }
 
@@ -125,4 +206,17 @@ public struct BiomeType
     public string name;
     public float temperature;
     public Color color;
+}
+
+public struct MapData {
+    public readonly float[,] heightMap;
+    public readonly float[,] temperatureMap;
+    public readonly Color[] colorMap;
+
+    public MapData(float[,] heightMap, float[,] temperatureMap, Color[] colorMap)
+    {
+        this.heightMap = heightMap;
+        this.temperatureMap = temperatureMap;
+        this.colorMap = colorMap;
+    }
 }
